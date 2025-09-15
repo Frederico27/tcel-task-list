e910ff8849c4674dc72ff202725f4ca803634c15
Frederico27 <fredericomarcal27@gmail.com>
2025-09-16 08:32:23 +0900

    first push

---
+++ database/migrations/2025_09_11_020823_create_documents_period.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents_period', function (Blueprint $table) {
+            $table->id('id_docu_period');
+            $table->unsignedBigInteger('id_documents');
+            $table->enum('period_type', ['daily', 'weekly', 'yearly', 'date']);
+            $table->string('period_value');
+            $table->timestamps();
+
+            $table->foreign('id_documents')
+                ->references('id_documents')->on('type_documents')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents_period');
+    }
+};
---
+++ database/migrations/2025_09_11_072658_create_pending_task.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pending_task', function (Blueprint $table) {
+            $table->id('id_pending_task');
+            $table->unsignedBigInteger('id_documents');
+            $table->date('periode_date');
+            $table->string('upload', 255);
+            $table->enum('status', [
+                'waiting',
+                'approved',
+                'rejected'
+            ])->default('waiting');
+            $table->string('approved_by')->nullable();
+            $table->timestamps();
+
+            $table->foreign('id_documents')
+                ->references('id_documents')->on('type_documents')
+                ->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pending_task');
+    }
+};
