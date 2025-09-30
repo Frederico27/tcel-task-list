302729a05429806571f22510e6279f924eaa9307
Frederico27 <fredericomarcal27@gmail.com>
2025-09-30 11:45:59 +0900

    fix topbar menu

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -50,7 +50,7 @@
                     </li>
 
                     <!-- Nav Item - Alerts -->
-                    <li class="nav-item dropdown no-arrow mx-1">
+                    {{-- <li class="nav-item dropdown no-arrow mx-1">
                         <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="fas fa-bell fa-fw"></i>
@@ -98,9 +98,9 @@
                             </a>
                             <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
                         </div>
-                    </li>
+                    </li> --}}
 
-                    <!-- Nav Item - Messages -->
+                    {{-- <!-- Nav Item - Messages -->
                     <li class="nav-item dropdown no-arrow mx-1">
                         <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -161,7 +161,7 @@
                             </a>
                             <a class="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
                         </div>
-                    </li>
+                    </li> --}}
 
                     <div class="topbar-divider d-none d-sm-block"></div>
 
@@ -175,7 +175,7 @@
                         <!-- Dropdown - User Information -->
                         <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                             aria-labelledby="userDropdown">
-                            <a class="dropdown-item" href="#">
+                            {{-- <a class="dropdown-item" href="#">
                                 <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                 Profile
                             </a>
@@ -187,7 +187,7 @@
                                 <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
                                 Activity Log
                             </a>
-                            <div class="dropdown-divider"></div>
+                            <div class="dropdown-divider"></div> --}}
                             <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                                 <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                 Logout
@@ -319,7 +319,7 @@
                                 <label for="type_periods" class="form-label">Type Periods</label>
                                 <select class="form-control" id="type_periods" name="type_periods" required>
                                     <option value="default" disabled selected>Select period type</option>
-                                    <option value="Daily">Daily</option>
+                                    {{-- <option value="Daily">Daily</option> --}}
                                     <option value="Weekly">Weekly</option>
                                     <option value="Yearly">Yearly</option>
                                 </select>
@@ -442,7 +442,7 @@
                                 <label for="edit_type_periods" class="form-label">Type Periods</label>
                                 <select class="form-control" id="edit_type_periods" name="type_periods" required>
                                     <option value="default" disabled selected>Select period type</option>
-                                    <option value="Daily">Daily</option>
+                                    {{-- <option value="Daily">Daily</option> --}}
                                     <option value="Weekly">Weekly</option>
                                     <option value="Yearly">Yearly</option>
                                 </select>
