0a59b682a55b5527b7d41dc36fbe0af5e11a3b0a
Frederico27 <fredericomarcal27@gmail.com>
2025-10-13 14:42:39 +0900

    fix name session

--- app/Http/Middleware/SuperadminMiddleware.php
+++ app/Http/Middleware/SuperadminMiddleware.php
@@ -19,7 +19,7 @@ class SuperadminMiddleware
         $sso_data = session('sso_user');
 
         if ($sso_data['nik'] != config('services.e-portal.admin_nik')) {
-            return response("You are not authorized to access this resource.", 401);
+            return redirect('https://e-portal.telkomcel.tl/app/ext/tasklist/audit/user');
         }
 
         return $next($request);
