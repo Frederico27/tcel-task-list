6bb31b9d22762bf5148c538f67dc527c3cac903e
Frederico27 <fredericomarcal27@gmail.com>
2025-10-17 16:54:26 +0900

    fix user controller forget nik comment

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,8 +17,8 @@ class UserController extends Controller
         $status = collect();
 
         try {
-            // $nik = session('sso_user')['nik'];
-            $nik = '4444';
+            $nik = session('sso_user')['nik'];
+            // $nik = '4444';
             // PIC is stored as JSON on the related `documents` table (Documents.pic)
             // PendingTask relation name is `document()` (singular) so use that
             $docs = PendingTask::with('document')
